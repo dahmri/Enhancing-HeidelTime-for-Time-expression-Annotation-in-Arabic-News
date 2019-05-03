d51e8780a25b28cf61379b889eff88a970126fa7
dahmri <dahmrik@gmail.com>
2019-05-03 10:44:47 +0200

    Update Sentences.java

--- src/Sentences.java
+++ src/Sentences.java
@@ -5,7 +5,7 @@ public class Sentences {
 	
 	int nb_word;
 	int nb_short_time_expression;
-    ArrayList<Word_Tag> list_of_WordTag = new ArrayList <Word_Tag> () ;
+    	ArrayList<Word_Tag> list_of_WordTag = new ArrayList <Word_Tag> () ;
 	int[] time_expression_index = new int[100];
 	ArrayList<Time_expression_position> TEP = new ArrayList <Time_expression_position> () ;
 	
